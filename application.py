1e9e50fbb5522d807ff62bd04960ba070e3c0885
Ippokratis Karakotsoglou <Ippokratis Karakotsoglou>
2016-04-01 01:05:44 +0200

    going on

--- application.py
+++ application.py
@@ -1,14 +1,23 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+#from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+#manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello Ippokratis! This is a Test</h1>'
+    return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>' % name
+    return render_template('user.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #manager.run()
